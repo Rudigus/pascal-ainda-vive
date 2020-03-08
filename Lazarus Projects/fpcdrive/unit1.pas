c8773a6073ecc10a8f50ffc35a363e16ed47a8ef
Rudigus <rodrigo.matos9@hotmail.com>
2020-03-08 13:26:40 -0300

    maybe in the future project

---
+++ Lazarus Projects/fpcdrive/unit1.pas
@@ -0,0 +1,30 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, lNetComponents;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    LHTTPClientComponent1: TLHTTPClientComponent;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
